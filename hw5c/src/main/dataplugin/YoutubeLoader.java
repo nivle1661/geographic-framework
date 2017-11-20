b12ce3cdf1ca5cec0b7b77758e8b8483ea1c86b8
Elvin Gu <egu@andrew.cmu.edu>
2017-11-20 04:05:49 -0500

    oops

--- hw5c/src/main/dataplugin/YoutubeLoader.java
+++ hw5c/src/main/dataplugin/YoutubeLoader.java
@@ -1,20 +1,12 @@
 package main.dataplugin;
 
-import edu.cmu.cs.cs214.hw5.framework.core.MusicData;
+import edu.cmu.cs.cs214.hw5.framework.core.DataLoader;
 
 import java.io.File;
-import java.io.IOException;
 
-public class YoutubeLoader extends MusicData {
-  /**
-   * Creates a new MusicData object
-   *
-   * @param file the File to the WAV file for this song
-   * @param name the String name of the song
-   * @throws IOException if the stream made from the file is not valid
-   *                     or there is some other error in reading the stream
-   */
-  public YoutubeLoader(File file, String name) throws IOException {
-    super(file, name);
+public class YoutubeLoader implements DataLoader {
+  @Override
+  public File onSelect() {
+    return null;
   }
 }
